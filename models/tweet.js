64260ae70b149aba0c5f567b6ac872b59375f163
Wilson <p780729@hotmail.com>
2020-05-26 22:55:42 +0800

    Set the associate on models and test models Successfully

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    description: DataTypes.TEXT,
+    UserId: DataTypes.INTEGER
   }, {});
-  Tweet.associate = function(models) {
+  Tweet.associate = function (models) {
+    Tweet.hasMany(models.Like)
+    Tweet.hasMany(models.Reply)
+    Tweet.belongsTo(models.User)
   };
   return Tweet;
 };
--- models/user.js
+++ models/user.js
@@ -1,8 +1,31 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
+    User.hasMany(models.Like)
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+
+    // 可以使用 user.Followers() 方法，根據被追隨的人找出追隨者
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+
+    // 根據追隨的人找出被他追隨的人
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
   };
   return User;
 };
