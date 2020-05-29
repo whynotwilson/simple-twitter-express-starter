31f0eabd00f721435c20b2bd1908f99560281def
Jerry Lai <jerrydavid123@hotmail.com>
2020-05-29 23:19:17 +0800

    Finish getTweets page

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,8 @@
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
+const Like = db.Like
+const Reply = db.Reply
 const moment = require('moment')
 
 const userController = {
@@ -10,18 +12,25 @@ const userController = {
 
     try {
       const userId = req.params.id
-      const { dataValues } = await User.findByPk(userId) ? await User.findByPk(userId) : null
+      const { dataValues } = await User.findByPk(userId) ? await User.findByPk(userId, {
+        include: [
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' },
+          Like,
+          Tweet,
+          Reply
+        ]
+      }) : null
 
       if (!dataValues) {
         throw new Error("user is not found")
       }
 
-      const tweets = await Tweet.findAll({ raw: true, next: true, where: { UserId: userId } }).map(tweet => ({
+      const tweets = dataValues.Tweets.map(tweet => ({
         ...tweet,
         description: tweet.description ? tweet.description.substring(0, 50) : null,
         updatedAt: tweet.updatedAt ? moment(tweet.updatedAt).format(`YYYY-MM-DD, hh:mm`) : '-'
       }))
-
 
       // let isOwn = userId === req.user.id ? true : false
 
