c9b2d4f0eb055bbc98d0280603f9d15ddd302bdd
Jerry Lai <jerrydavid123@hotmail.com>
2020-05-29 21:43:07 +0800

    Set view eninge and Add main, getTweets page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const helpers = require('./_helpers');
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
