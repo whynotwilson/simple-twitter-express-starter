a4f7529d5fa8ca7523d0290f020db1e92995f18b
Wilson <p780729@hotmail.com>
2020-05-30 21:20:51 +0800

    Add signin and signup feature

--- app.js
+++ app.js
@@ -2,24 +2,47 @@ const express = require('express')
 const helpers = require('./_helpers')
 const handlebars = require('express-handlebars')
 const userController = require('./controllers/userController')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const passport = require('./config/passport')
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.engine('handlebars', handlebars({
   defaultLayout: 'main'
   // helpers: require('./config/handlebars-helpers')
 }))
-
 app.set('view engine', 'handlebars')
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/signup', userController.signUpPage)
-// app.post('/signup', userController.signUp)
+app.post('/signup', userController.signUp)
 app.get('/signin', userController.signInPage)
-// app.post('/signin', userController.signIn)
+app.post('/signin', userController.signIn)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
