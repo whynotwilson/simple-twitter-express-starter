fa88fabcd2bc6ec21d967ef544db2afdcf060180
Jerry Lai <jerrydavid123@hotmail.com>
2020-06-02 21:52:08 +0800

    Initial tagFriend feature and add function to test on tweets page

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('view engine', 'handlebars')
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(express.static('public'))
 
 app.use(passport.initialize())
 app.use(passport.session())
